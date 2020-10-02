07c978529e6fc77fb012ebc11280e082d4690cb6
Martin <m87.wheeler@gmail.com>
2020-10-02 07:14:23 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<style type="text/scss">
+  @import './styles/theme.scss';
+  @import './styles/typography.scss';
+</style>
+
+<h1 class="header">Hello World</h1>
+<p>
+  Eiusmod proident sit laboris proident non tempor aliquip. Magna occaecat
+  officia fugiat ad minim sit. In exercitation et duis id sunt magna enim eu
+  pariatur aliquip excepteur reprehenderit irure et. Amet cupidatat nostrud
+  nulla cupidatat pariatur esse aliqua mollit. Excepteur consectetur Lorem
+  occaecat do ipsum incididunt deserunt. Irure ea cillum laboris eu elit
+  occaecat quis labore ut excepteur ullamco magna. Sunt fugiat aliquip aute
+  magna nisi culpa consequat ullamco dolor id.
+</p>
